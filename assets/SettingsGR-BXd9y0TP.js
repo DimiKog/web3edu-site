6cfa1c5599e9fe1ea5bc466f43388fb233297e37
DimiKog <dimitris@kogias.eu>
2026-08-29 13:43:14 +0300

    Updates

--- assets/SettingsGR-BXd9y0TP.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./vendor-react-DbODGLeV.js";import r from"./Settings-BKX3_0BE.js";import"./index-DicmcLBt.js";import"./vendor-web3-DeSYFzHo.js";import"./identityExport-0RG2FTks.js";import"./ExclamationTriangleIcon-BMNNgg-q.js";function e(){return t.jsx(r,{lang:"gr"})}export{e as default};
---
+++ assets/index-CCze81LO.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-CTtygi22.js";import{P as r}from"./Poe-B8VpeI3z.js";import"./index-Br_XfSAg.js";import"./vendor-web3-DteUQuq0.js";import"./theme-CllK3pJ1.js";function a(){return o.jsx(r,{language:"EN"})}export{a as default};
